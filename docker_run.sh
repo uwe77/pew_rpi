c224cf31a5456d1951e3988ff1a0f918600c9020
uwe77 <uwe90711@gmail.com>
2023-12-12 19:14:18 +0800

    init repo

---
+++ docker_run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+   else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+
+#Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+   exit 1
+fi
+
+docker run -it \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=$XAUTH \
+    -v "$XAUTH:$XAUTH" \
+    -v "/home/$USER/raspi:/home/pew/raspi" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v "/home/$USER/.bashrc:/home/pew/.bashrc" \
+    --workdir "/home/pew/raspi" \
+    --user "root:root" \
+    --name pew \
+    --network host \
+    --rm \
+    --privileged \
+    raspi/dogg:lastest \
+    bash 
---
+++ environment.sh
@@ -0,0 +1,2 @@
+source /opt/ros/melodic/setup.bash
+source ./catkin_ws/devel/setup.bash
