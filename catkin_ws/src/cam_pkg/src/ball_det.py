991926e04e5120ffcf27ecd38f0764593e9cdd05
uwe77 <uwe90711@gmail.com>
2023-12-27 23:20:33 +0800

    aaaa

--- catkin_ws/src/cam_pkg/src/ball_det.py
+++ catkin_ws/src/cam_pkg/src/ball_det.py
@@ -8,13 +8,16 @@ from std_msgs.msg import Int32MultiArray
 IMAGE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                           '../../../images')
 FILE_NAME = "vision"
-lower_pink = np.array([140, 100, 50])
-upper_pink = np.array([170, 255, 255])
+lower_pink = np.array([140, 100, 50]) # 140 100 50
+upper_pink = np.array([170, 255, 255]) # 170, 255, 255
 
 def main():
     rospy.init_node('ball_det')
     ball_pos_pub = rospy.Publisher('ball_center', Int32MultiArray, queue_size=1)
     count = 0
+    min_R = 10
+    max_R = 150
+    last_R = 0
     while not rospy.is_shutdown():
         try:
             count = (count % 2) + 1
@@ -33,14 +36,29 @@ def main():
             mask = cv2.inRange(captured_frame_hsv, lower_pink, upper_pink)
             mask = cv2.GaussianBlur(mask, (5, 5), 2)
             # cv2.imwrite("../../../images/mask.png", mask)
+            level = last_R // 100
+            if level == 0:
+                min_R = 20
+                max_R = 200
+            elif level == 1:
+                min_R = 50
+                max_R = 250
+            elif level == 2:
+                min_R = 150
+                max_R = 350
+            elif level >= 3:
+                min_R = 250
+                max_R = 450
             circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=2, minDist=mask.shape[0] / 8,
-                                    param1=100, param2=18, minRadius=30, maxRadius=300)
+                                    param1=100, param2=18, minRadius=min_R, maxRadius=max_R)
             pos_msg = Int32MultiArray()
             if circles is not None:
                 circles = np.round(circles[0, :]).astype("int")
                 pos_msg.data = [circles[0, 0], circles[0, 1], circles[0, 2]]
+                last_R = circles[0, 2]
             else:
                 pos_msg.data = [0, 0, 0]
+                last_R = 0
             ball_pos_pub.publish(pos_msg)
         except:
             pass
--- catkin_ws/src/dog_pkg/src/track_ball.py
+++ catkin_ws/src/dog_pkg/src/track_ball.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Int32, Float32, Int32MultiArray
+from std_msgs.msg import Int32, Float32, Int32MultiArray, Bool
 import math
 from math import pi
 
@@ -13,38 +13,60 @@ last_theta = 0
 speed = 50
 BOUND_X = 640
 BOUND_Y = 480
+ball_catch = False
 def ball_center_callback(msg):
     global ball_x, ball_y, ball_r, theta, last_theta, speed, BOUND_X
-    if msg.data[2] != 0:
-        ball_x, ball_y, ball_r = msg.data[:3]
-        theta = math.acos((ball_x-BOUND_X/2)/(BOUND_X/2))
-        if abs(abs(theta) - pi/2) <= pi/24:
-            # if ball_r < 200:
-            #     speed = 100/199 * (200 - ball_r)
-            # else:
-            #     speed = 0
-            speed = 100/270*(300-ball_r) if ball_r < 200 else 0
-            theta = pi/2
-        else:
-            speed = 50/270*(300-ball_r) if ball_r < 200 else 0
-    else:
-        speed = 10
-        oper = (last_theta - pi / 2) / abs(last_theta - pi / 2) if last_theta != pi / 2 else 1
-        theta = (oper + 1)*pi/2
-    print(theta*180/pi, speed, msg.data[2])
-def main():
-    global ball_x, ball_y, theta, speed, BOUND_X, last_theta
+    ball_x, ball_y, ball_r = msg.data[:3]
+
+def track_ball():
+    global ball_x, ball_y, ball_r, theta, speed, BOUND_X, last_theta, ball_catch
     theta = pi/2
     speed = 0
     ball_x = 0
+    ball_r = 0
+    ball_catch = False
+    count = 0
     rospy.init_node('track_ball')
     speed_pub = rospy.Publisher('dog_move_speed', Int32, queue_size=1)
     theta_pub = rospy.Publisher('dog_move_dir', Float32, queue_size=1)
+    catch_pub = rospy.Publisher('dog_catch_ball', Bool, queue_size=1)
     rospy.Subscriber('ball_center', Int32MultiArray, ball_center_callback)
     while not rospy.is_shutdown():
+        if ball_r != 0 and not ball_catch:
+            if ball_r < 300:
+                theta = math.acos((ball_x-BOUND_X/2)/(BOUND_X/2))
+                if abs(abs(theta) - pi/2) <= pi/24:
+                    speed = 100/340*(350-ball_r)
+                    theta = pi/2
+                else:
+                    speed = 50/340*(350-ball_r)
+            else:
+                speed = 100
+                theta = pi/2
+                ball_catch = True
+        elif ball_r == 0 and not ball_catch:
+            speed = 10
+            oper = (last_theta - pi / 2) / abs(last_theta - pi / 2) if last_theta != pi / 2 else 1
+            theta = (oper + 1)*pi/2
+        
+        if ball_catch:
+            if count >= 5:
+                catch_pub.publish(ball_catch)
+                theta = 0
+                speed = 10
+                if ball_r != 0:
+                    count = 0
+                    ball_catch = False
+            else:
+                count += 1
+        else:
+            catch_pub.publish(ball_catch)
+
+        print(theta*180/pi, speed, ball_r, ball_catch)
         theta_pub.publish(float(theta))
         last_theta = theta
         speed_pub.publish(int(speed))
+        
         rospy.sleep(0.1)
 if __name__ == '__main__':
-    main()
+    track_ball()
